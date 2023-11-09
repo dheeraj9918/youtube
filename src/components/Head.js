017d9edab3bf933e5bc693ce9225ea778a506052
Dheeraj Rauniyar <dheerajrauniyar123@gmail.com>
2023-11-09 11:36:17 +0530

    making dynamic live comment component

--- src/components/Head.js
+++ src/components/Head.js
@@ -4,90 +4,95 @@ import { useEffect, useState } from "react";
 import { YOUTUBE_SUGGESTION_API } from "../utils/constant";
 import { cacheResult } from "../utils/searchSlice";
 
-
-
 const Head = () => {
-    const [searchQuery, setSearchQuery] = useState("");
-    const [suggesitions, setSuggesitions] = useState([]);
-    const [showSeggesitions, setShowSeggesitions] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
 
-    const searchCashes = useSelector(store => store.search);
+  const searchCaches = useSelector((store) => store.search);
 
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            if (searchCashes[searchQuery]) {
-                setSuggesitions(searchCashes[searchQuery]);
-            }
-            else {
-                getSearchSuggestionsApi();
-            }
-        }, 200);
-        return () => {
-            clearTimeout(timer);
-        }
-    }, [searchQuery]);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (searchCaches[searchQuery]) {
+        setSuggestions(searchCaches[searchQuery]);
+      } else {
+        getSearchSuggestionsApi();
+      }
+    }, 200);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchQuery]);
 
-    const getSearchSuggestionsApi = async () => {
-        const data = await fetch(YOUTUBE_SUGGESTION_API + searchQuery);
-        const json = await data.json();
-        // console.log(json);
-        setSuggesitions(json[1]);
-        dispatch(
-            cacheResult({
-                [searchQuery]:json[1],
-            })
-        )
+  const getSearchSuggestionsApi = async () => {
+    const data = await fetch(YOUTUBE_SUGGESTION_API + searchQuery);
+    const json = await data.json();
+    setSuggestions(json[1]);
+    dispatch(
+      cacheResult({
+        [searchQuery]: json[1],
+      })
+    );
+  };
 
-    }
+  const dispatch = useDispatch();
+  const toggleHandlerMenu = () => {
+    dispatch(toggleMenu());
+  };
 
-    //this dispatch function is for hamburger;
-    const dispatch = useDispatch();
-    const toggleHndlerMenu = () => {
-        dispatch(toggleMenu());
-    }
-
-
-    return (
-        <div className='grid grid-flow-col p-4 m-2 shadow-md '>
-            <div className='flex col-span-1'>
-                <img
-                    onClick={() => toggleHndlerMenu()}
-                    className='h-8 mx-5 cursor-pointer'
-                    alt="menu"
-                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/2048px-Hamburger_icon.svg.png" />
-
-                <img className='h-8 cursor-pointer'
-                    alt="youtube-logo"
-                    src="https://logodownload.org/wp-content/uploads/2014/10/youtube-logo-9.png" />
-
-            </div>
-            <div className='col-span-10 ml-36'>
-                <div>
-                    <input
-                        className='w-1/2 border border-gray-600  p-2 rounded-l-full'
-                        type="text"
-                        value={searchQuery}
-                        onChange={(e) => setSearchQuery(e.target.value)}
-                        onFocus={() => setShowSeggesitions(true)}
-                        onBlur={() => setShowSeggesitions(false)}
-                    />
-                    <button className='border border-gray-600 border-l-transparent p-2 rounded-r-full bg-gray-100'>Search</button>
-                </div>
-                {showSeggesitions && (<div className="fixed bg-white w-[29.25rem] rounded-lg border">
-                    <ul>
-                        {suggesitions.map((s, i) => <li key={i} className="py-2 text-lg mx-2w-[29.25rem] hover:bg-gray-100">🔍 {s}</li>)}
-
-                    </ul>
-                </div>)}
-            </div>
-            <div >
-                <img
-                    className='h-10'
-                    alt="user"
-                    src="https://cdn-icons-png.flaticon.com/512/149/149071.png" />
-            </div>
+  return (
+    <div className="grid grid-flow-col p-4 m-2 shadow-md">
+      <div className="flex col-span-1">
+        <img
+          onClick={toggleHandlerMenu()}
+          className="h-8 mx-5 cursor-pointer"
+          alt="menu"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/2048px-Hamburger_icon.svg.png"
+        />
+        <img
+          className="h-8 cursor-pointer"
+          alt="youtube-logo"
+          src="https://logodownload.org/wp-content/uploads/2014/10/youtube-logo-9.png"
+        />
+      </div>
+      <div className="col-span-10 ml-36">
+        <div>
+          <input
+            className="w-1/2 border border-gray-600 p-2 rounded-l-full"
+            type="text"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            onFocus={() => setShowSuggestions(true)}
+            onBlur={() => setShowSuggestions(false)}
+          />
+          <button className="border border-gray-600 border-l-transparent p-2 rounded-r-full bg-gray-100">
+            Search
+          </button>
         </div>
-    )
-}
+        {showSuggestions && (
+          <div className="fixed bg-white w-[29.25rem] rounded-lg border">
+            <ul>
+              {suggestions.map((s, i) => (
+                <li
+                  key={i}
+                  className="py-2 text-lg mx-2w-[29.25rem] hover:bg-gray-100"
+                >
+                  🔍 {s}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+      <div>
+        <img
+          className="h-10"
+          alt="user"
+          src="https://cdn-icons-png.flaticon.com/512/149/149071.png"
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Head;
--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,42 +1,64 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react';
 import ChatMessage from './ChatMessage';
 import { useSelector, useDispatch } from 'react-redux';
 import { addmessages } from '../utils/chatSlice';
 import { generateRandomName, randomMessageGenerator } from '../utils/helper';
 
-
 const LiveChat = () => {
+  const [isLiveMessage, setIsLiveMessage] = useState('');
+  const dispatch = useDispatch();
+  const chatMessage = useSelector(store => store.chat.messages);
 
-    const dispatch = useDispatch();
-    const chatMessage = useSelector(store => store.chat.messages);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      dispatch(
+        addmessages({
+          name: generateRandomName(),
+          message: randomMessageGenerator(25) + ' 🚀',
+        })
+      );
+    }, 1500);
 
-    useEffect(() => {
-        const i = setInterval(() => {
-            //api polling 
-            console.log('api polling');
-            dispatch(
-                addmessages({
-                    name: generateRandomName(),
-                    message: randomMessageGenerator(25) + " 🚀",
-                })
-            )
-        }, 500);
+    return () => clearInterval(intervalId);
+  }, []);
 
-        return () => clearInterval(i);
-
-    }, []);
-
-    return (
-        <div className='border border-gray-400 [600px] rounded-md ml-2'>
-            <div className='bg-gray-100 rounded-md p-2 w-full h-[550px]  overflow-y-scroll flex flex-col-reverse'>
-                {chatMessage.map((c, i) => (<ChatMessage key={i} name={c.name} message={c.message} />))}
-            </div>
-            <div className='w-full   ml-2 p-2 rounded-md'>
-                <input className='w-[350px] p-[3px] border border-gray-400 rounded-md' type="text"/>
-                <button className='bg-blue-400 ml-2 p-[3px] w-12 rounded-md'>Send</button>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className='border border-gray-400 h-[600px] rounded-md ml-2'>
+      <div className='bg-gray-100 rounded-md p-2 w-full h-[550px] overflow-y-scroll flex flex-col-reverse'>
+        {chatMessage.map((c, i) => (
+          <ChatMessage key={i} name={c.name} message={c.message} />
+        ))}
+      </div>
+      <form
+        className='w-full flex flex-wrap items-center ml-2 rounded-b-md'
+        onSubmit={e => {
+          e.preventDefault();
+          dispatch(
+            addmessages({
+              name: 'Dheeraj Rauniyar',
+              message: isLiveMessage,
+            })
+          );
+          setIsLiveMessage('');
+        }}
+      >
+        <input
+          value={isLiveMessage}
+          onChange={e => {
+            setIsLiveMessage(e.target.value);
+          }}
+          className='w-[350px] p-[3px] px-2 border border-gray-400 rounded-md'
+          type='text'
+        />
+        <button
+          className='bg-blue-400 ml-2 p-[3px] w-12 rounded-md'
+          type='submit'
+        >
+          Send
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default LiveChat;
